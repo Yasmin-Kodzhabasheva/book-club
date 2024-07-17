b2acb0e47de42bd0dfdb072f9b29c563350538e0
Kodjabashevayasemin@gmail.com <Kodjabashevayasemin@gmail.com>
2024-07-17 16:33:00 +0300

    Add the option to change the content of the form

--- src/components/FormComponent.ts
+++ src/components/FormComponent.ts
@@ -1,63 +1,100 @@
-import { HTMLTemplate } from "../utils/utils.ts";
-import { auth } from "../firebase/firebase.ts";
+import { HTMLTemplate, Auth } from "../utils/utils.ts";
 
 class FormComponent {
     private formElement: HTMLFormElement;
+    private formType: string;
+
+    private registerContent = `
+    <div class="form-email">
+            <div class="email">
+                <label for="email">Enter your email</label>
+                <input type="email" name="email" id="email" required />
+            </div>
+
+            <div class="confirm-email">
+                <label for="confirm-email">Confirm your email</label>
+                <input
+                    type="email"
+                    name="confirm-email"
+                    id="confirm-email"
+                    required
+                />
+            </div>
+        </div>
+
+        <div class="form-password">
+            <div class="password">
+                <label for="password">Enter your password</label>
+                <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    required
+                />
+            </div>
+
+            <div class="confirm-password">
+                <label for="confirm-password">
+                    Confirm your password
+                </label>
+
+                <input
+                    type="text"
+                    name="confirm-password"
+                    id="confirm-password"
+                    required
+                />
+            </div>
+        </div>
+        <button>Register</button>
+        <div class="error-message">
+            <p></p>
+        </div>
+    `;
+
+    private loginContent = `
+        <div class="login-email">
+        <label for="email">Enter your email</label>
+        <input type="email" name="email" id="email" required />
+        </div>
+
+        <div class="login-password">
+        <label for="password">Enter your password</label>
+        <input
+            type="text"
+            name="password"
+            id="password"
+            required
+        />
+        </div>
+        <button>Login</button>
+    `;
 
     constructor() {
-        HTMLTemplate.insertContent(
-            "#app",
-            `<form>
-          <div class="form-email">
-              <div class="email">
-                  <label for="email">Enter your email</label>
-                  <input type="email" name="email" id="email" required />
-              </div>
-
-              <div class="confirm-email">
-                  <label for="confirm-email">Confirm your email</label>
-                  <input
-                      type="email"
-                      name="confirm-email"
-                      id="confirm-email"
-                      required
-                  />
-              </div>
-          </div>
-
-          <div class="form-password">
-              <div class="password">
-                  <label for="password">Enter your password</label>
-                  <input
-                      type="text"
-                      name="password"
-                      id="password"
-                      required
-                  />
-              </div>
-
-              <div class="confirm-password">
-                  <label for="confirm-password">
-                      Confirm your password
-                  </label>
-
-                  <input
-                      type="text"
-                      name="confirm-password"
-                      id="confirm-password"
-                      required
-                  />
-              </div>
-          </div>
-          <button>Register</button>
-          <div class="error-message">
-              <p></p>
-          </div>
-      </form>`
-        );
+        this.formType = "register";
+        this.initializeForm(this.formType);
         this.formElement = document.querySelector("form")!;
-
         this.formElement.addEventListener("submit", this.submitForm.bind(this));
+    }
+
+    initializeForm(type: string) {
+        this.formType = type;
+
+        switch (this.formType) {
+            case "login":
+                HTMLTemplate.insertMainContent("#app form", this.loginContent);
+                break;
+            case "register":
+                const params = {
+                    insertEl: "#app",
+                    content: this.registerContent,
+                    createEl: "form",
+                };
+
+                HTMLTemplate.insertContent(params);
+                break;
+        }
+
     }
 
     showErrorMsg(message: string) {
@@ -112,21 +149,32 @@ class FormComponent {
 
     submitForm(event: Event) {
         event.preventDefault();
+
+        switch (this.formType) {
+            case "login":
+                this.login();
+                break;
+            case "register":
+                this.register();
+                break;
+        }
+    }
+
+    register() {
         const input = this.validateInput();
 
         if (!input) return;
 
-        auth.createUserWithEmailAndPassword(input.email, input.password)
-            .then((userCredential) => {
-                // Signed up
-                const user = userCredential.user;
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-            });
+        Auth.createUser(input.email, input.password);
+    }
+
+    login() {
+        const formData = new FormData(this.formElement);
+
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+
+        Auth.authUser(email, password)
     }
 }
 
