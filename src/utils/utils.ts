28d7d36ffa31dd99a85f6fe958b7fde89c9499aa
Kodjabashevayasemin@gmail.com <Kodjabashevayasemin@gmail.com>
2024-07-17 16:34:24 +0300

    Create helper classes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,76 @@
+import { auth } from "../firebase/firebase";
+import LandingComponent from "../components/LandingComponent";
+import HomeComponent from "../components/HomeComponent";
+
+class UI {
+    static render(verification: string) {
+        if (Auth.isAuth() || verification) {
+            new HomeComponent();
+        } else {
+            new LandingComponent();
+        }
+    }
+}
+
 class HTMLTemplate {
-    static insertContent(element: string, content: string) {
+    static insertMainContent(element: string, content: string) {
         const rootElement = document.querySelector(element)!;
 
         rootElement.innerHTML = content;
     }
+
+    static insertContent(params: {
+        insertEl: string;
+        content: string;
+        createEl: string;
+    }) {
+        const rootElement = document.querySelector(params.insertEl)!;
+        const createdEl = document.createElement(params.createEl);
+
+        createdEl.innerHTML = params.content;
+        rootElement.appendChild(createdEl);
+    }
 }
 
-export { HTMLTemplate };
+class Auth {
+    static isAuth() {
+        const cookies = document.cookie;
+
+        if (cookies.includes("userAuth=true")) return true;
+
+        return false;
+    }
+
+    static createUser(email: string, password: string) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed up
+                const user = userCredential.user;
+                document.cookie = "userAuth=true";
+                UI.render("auth");
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
+
+    static authUser(email: string, password: string) {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                document.cookie = "userAuth=true";
+                UI.render("auth");
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });
+    }
+}
+
+export { HTMLTemplate, Auth, UI };
